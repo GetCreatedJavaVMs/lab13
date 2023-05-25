3a13b4a564476a6b1ed79f4407c232a0ae25c869
GetCreatedJavaVMs <130655015+GetCreatedJavaVMs@users.noreply.github.com>
2023-05-25 12:55:58 +0300

    Add files via upload

---
+++ RomanStringLibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RomanStringLibraryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RomanStringLibraryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a8bec51-2978-4928-9d91-f31d40b4af28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RomanStringLibraryTest/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RomanLibrary;
+
+namespace RomanStringLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+            // Тест который должен вернуть значение true.
+            string[] words = { "Alphabet", "Zebra", "ABC", "Москва" };
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsTrue(result, string.Format($"Ожидается для {word}': true; В действительности: {result}"));
+            }
+        }
+
+
+        [TestMethod]
+        public void TestDoesNotStartWithUpper()
+        {
+            // Tests that we expect to return false.
+            string[] words = { "alphabet", "zebra", "abc", "государство" };
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsFalse(result,
+           string.Format($"Ожидается для {word}: true; в действительности получаем: {result}"));
+            }
+        }
+
+
+        [TestMethod]
+        public void DirectCallWithNullOrEmpty()
+        {
+            // Tests that we expect to return false.
+            string[] words = { string.Empty, null };
+            foreach (var word in words)
+            {
+                bool result = StringLibrary.StartsWithUpper(word);
+                Assert.IsFalse(result,
+                string.Format($"Ожидается для {word}: true; в действительности получаем: {result}"));
+            }
+        }
+
+    }
+}
